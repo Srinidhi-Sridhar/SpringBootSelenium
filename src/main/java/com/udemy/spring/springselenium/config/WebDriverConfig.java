7ec8c82853e921d2ec569c069d8c7f71e027015d
Srinidhi <srinidhi.sridharks@gmail.com>
2021-12-13 12:07:25 +0530

    Removed Unwanted Stuff Added New Files to Support Remote Webdriver testing Refactored Code

--- src/main/java/com/udemy/spring/springselenium/config/WebDriverConfig.java
+++ src/main/java/com/udemy/spring/springselenium/config/WebDriverConfig.java
@@ -5,15 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import java.time.Duration;
 
 @Configuration
+@Profile("!remote")
 public class WebDriverConfig {
 
     @Value("${chromeDriverVersion}")
@@ -49,8 +51,4 @@ public class WebDriverConfig {
         return new ChromeDriver();
     }
 
-    @Bean
-    public WebDriverWait webDriverWait(WebDriver driver) {
-        return new WebDriverWait(driver, timeOut);
-    }
 }
--- src/test/java/com/udemy/spring/springselenium/SpringseleniumApplicationTests.java
+++ src/test/java/com/udemy/spring/springselenium/SpringseleniumApplicationTests.java
@@ -1,15 +1,7 @@
 package com.udemy.spring.springselenium;
 
-import com.github.javafaker.Faker;
-import com.udemy.spring.springselenium.config.GetList;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 @SpringBootTest
 class SpringseleniumApplicationTests {
-
 }

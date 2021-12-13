d2e94600b3656db4a13380b3d7828d7b2aab9bb2
Srinidhi <srinidhi.sridharks@gmail.com>
2021-12-13 16:18:48 +0530

    Added Testng suitr ,and added a similar test to run in parallel

---
+++ src/test/java/com/udemy/spring/springselenium/Google2Test.java
@@ -0,0 +1,35 @@
+package com.udemy.spring.springselenium;
+
+import com.udemy.spring.springselenium.pages.google.GooglePage;
+import com.udemy.spring.springselenium.utils.UtilityFunctions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Google2Test extends BaseTest {
+
+    @Autowired
+    private GooglePage googlePage;
+
+    @Lazy
+    @Autowired
+    UtilityFunctions utilityFunctions;
+
+    @Test
+    public void searchTest() throws IOException {
+        googlePage.goTo();
+        Assert.assertTrue(googlePage.isLoaded());
+        googlePage.getSearchComponent().searchSomething("Java");
+        Assert.assertTrue(googlePage.getSearchResult().getCount() > 2);
+        utilityFunctions.takeScreenShot();
+    }
+
+    @AfterTest
+    public void closeDriver() {
+        googlePage.driver.quit();
+    }
+}
